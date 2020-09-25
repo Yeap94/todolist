760cb97852ecccc3d7c54a595d08c7f16e1663d4
Yeap94 <iioiiieji@gmail.com>
2020-09-26 01:39:07 +0300

    fix styles

--- src/app/directives/navbardirective/navbar.directive.ts
+++ src/app/directives/navbardirective/navbar.directive.ts
@@ -1,11 +1,8 @@
 import * as angular from 'angular';
-import { NavBarDirectiveCtrl } from './components/navbar.component';
 
 export const selector: string = 'navbarDirective';
 export function NavbarDirective(): angular.IDirective {
     let navbarDirectiveFunc: angular.IDirective = {};
     navbarDirectiveFunc.template = require ('./components/navbar.component.html');
-    navbarDirectiveFunc.controller = NavBarDirectiveCtrl;
-    navbarDirectiveFunc.controllerAs = 'vm';
     return navbarDirectiveFunc;
 }
