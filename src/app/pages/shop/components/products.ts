2577843210a54c905266bc6d23776b3050deff56
Yeap94 <iioiiieji@gmail.com>
2020-09-27 23:05:20 +0300

    shop update

--- src/app/pages/shop/components/products.ts
+++ src/app/pages/shop/components/products.ts
@@ -1,44 +1,266 @@
 export const products = [
     {
-        name: 'Ball',
-        price: 145,
-        discountPrice: 145,
+        name: 'Acura',
+        price: 45000,
+        discountPrice: 45000,
+        priceChanged: false
+    },
+    {
+        name: 'Alfa romeo',
+        price: 40000,
+        discountPrice: 40000,
+        priceChanged: false
+    },
+    {
+        name: 'Aquarium',
+        price: 150,
+        discountPrice: 150,
+        priceChanged: false
+    },
+    {
+        name: 'Aston martin',
+        price: 55000,
+        discountPrice: 55000,
+        priceChanged: false
+    },
+    {
+        name: 'Audi a6',
+        price: 50000,
+        discountPrice: 50000,
+        priceChanged: false
+    },
+    {
+        name: 'Baseball',
+        price: 20,
+        discountPrice: 20,
+        priceChanged: false
+    },
+    {
+        name: 'Basketball',
+        price: 50,
+        discountPrice: 50,
+        priceChanged: false
+    },
+    {
+        name: 'Candy',
+        price: 3,
+        discountPrice: 3,
+        priceChanged: false
+    },
+    {
+        name: 'Ceed',
+        price: 12000,
+        discountPrice: 12000,
+        priceChanged: false
+    },
+    {
+        name: 'Chainsaw',
+        price: 200,
+        discountPrice: 200,
         priceChanged: false
     },
     {
         name: 'Chair',
-        price: 4587,
-        discountPrice: 4587,
+        price: 30,
+        discountPrice: 30,
         priceChanged: false
     },
     {
-        name: 'Computer',
+        name: 'Compass',
+        price: 11,
+        discountPrice: 11,
+        priceChanged: false
+    },
+    {
+        name: 'Conor',
+        price: 5000000,
+        discountPrice: 5000000,
+        priceChanged: false
+    },
+    {
+        name: 'Cricket',
+        price: 5,
+        discountPrice: 5,
+        priceChanged: false
+    },
+    {
+        name: 'Cube',
+        price: 3000,
+        discountPrice: 3000,
+        priceChanged: false
+    },
+    {
+        name: 'Dumplings',
+        price: 140,
+        discountPrice: 140,
+        priceChanged: false
+    },
+    {
+        name: 'Football',
+        price: 40,
+        discountPrice: 40,
+        priceChanged: false
+    },
+    {
+        name: 'Fried chicken',
+        price: 6,
+        discountPrice: 6,
+        priceChanged: false
+    },
+    {
+        name: 'Goat',
         price: 20567,
         discountPrice: 20567,
         priceChanged: false
     },
     {
-        name: 'Book',
-        price: 425,
-        discountPrice: 425,
-        priceChanged: false
-    },
-    {
-        name: 'Phone',
-        price: 784,
-        discountPrice: 784,
+        name: 'GTR',
+        price: 130000,
+        discountPrice: 130000,
         priceChanged: false
     },
     {
         name: 'House',
-        price: 251360,
-        discountPrice: 251360,
+        price: 300000,
+        discountPrice: 300000,
+        priceChanged: false
+    },
+    {
+        name: 'King chair',
+        price: 35,
+        discountPrice: 35,
+        priceChanged: false
+    },
+    {
+        name: 'Lambo',
+        price: 250000,
+        discountPrice: 250000,
+        priceChanged: false
+    },
+    {
+        name: 'Macbook',
+        price: 1400,
+        discountPrice: 1400,
+        priceChanged: false
+    },
+    {
+        name: 'Mazda 6',
+        price: 25000,
+        discountPrice: 25000,
+        priceChanged: false
+    },
+    {
+        name: 'Mazda cx5',
+        price: 31000,
+        discountPrice: 31000,
+        priceChanged: false
+    },
+    {
+        name: 'Office chair',
+        price: 9,
+        discountPrice: 9,
+        priceChanged: false
+    },
+    {
+        name: 'PC',
+        price: 500,
+        discountPrice: 500,
+        priceChanged: false
+    },
+    {
+        name: 'Phone',
+        price: 600,
+        discountPrice: 600,
+        priceChanged: false
+    },
+    {
+        name: 'Qashqai',
+        price: 18000,
+        discountPrice: 18000,
+        priceChanged: false
+    },
+    {
+        name: 'Samovar',
+        price: 1300,
+        discountPrice: 1300,
+        priceChanged: false
+    },
+    {
+        name: 'Skateboard',
+        price: 70,
+        discountPrice: 70,
+        priceChanged: false
+    },
+    {
+        name: 'Sorento',
+        price: 25000,
+        discountPrice: 25000,
+        priceChanged: false
+    },
+    {
+        name: 'Stinger',
+        price: 27500,
+        discountPrice: 27500,
+        priceChanged: false
+    },
+    {
+        name: 'Table',
+        price: 62.5,
+        discountPrice: 62.5,
+        priceChanged: false
+    },
+    {
+        name: 'Taburet',
+        price: 10,
+        discountPrice: 10,
         priceChanged: false
     },
     {
         name: 'TV',
-        price: 1300,
-        discountPrice: 1300,
+        price: 500,
+        discountPrice: 500,
+        priceChanged: false
+    },
+    {
+        name: 'Vaz 2105',
+        price: 875,
+        discountPrice: 875,
+        priceChanged: false
+    },
+    {
+        name: 'Vaz 2106',
+        price: 850,
+        discountPrice: 850,
+        priceChanged: false
+    },
+    {
+        name: 'Vaz 2107',
+        price: 912,
+        discountPrice: 912,
+        priceChanged: false
+    },
+    {
+        name: 'Vesta Grey',
+        price: 11500,
+        discountPrice: 11500,
+        priceChanged: false
+    },
+    {
+        name: 'Vesta White',
+        price: 11250,
+        discountPrice: 11250,
+        priceChanged: false
+    },
+    {
+        name: 'VIP House',
+        price: 400000,
+        discountPrice: 400000,
+        priceChanged: false
+    },
+    {
+        name: 'Whiskey',
+        price: 18,
+        discountPrice: 18,
         priceChanged: false
     }
 ];
