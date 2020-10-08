9f3ec83048a9436ce43e26d5c88ec9cf3cbeb1f3
Yeap94 <iioiiieji@gmail.com>
2020-10-09 00:21:53 +0300

    no sale timer

--- src/app/services/findminimal.service.ts
+++ src/app/services/findminimal.service.ts
@@ -9,25 +9,29 @@ export class FindMinimalPriceService {
     public static selector = 'FindMinimalPriceService';
     public minimalPrices: Array<IMinimal> = minimal;
     public topMinimal: IMinimal;
+    public counter: number = 0;
 
     public compare = (product: IProduct) => {
         let equalProduct: IMinimal = _.find(this.minimalPrices, (each: IMinimal) => angular.equals(each.name, product.name));
         if (equalProduct !== undefined) {
             let equalProductIndex: number = _.findIndex(this.minimalPrices, (each: IMinimal) => angular.equals(each.name, product.name));
             if (product.discountPrice < equalProduct.price) {
+                this.counter = 0;
                 this.minimalPrices[equalProductIndex].price = product.discountPrice;
                 this.minimalPrices[equalProductIndex].date = moment();
                 this.topMinimal =  this.minimalPrices[equalProductIndex];
-                // console.log('Top minimal in if:\t', this.topMinimal.name, '\t', Math.floor(this.topMinimal.price));
+            } else {
+               this.topMinimal = null;
+               this.counter++;
             }
         } else {
+            this.counter = 0;
             this.minimalPrices.push({
                 name: product.name,
                 price: product.discountPrice,
                 date: moment()
             });
             this.topMinimal = this.minimalPrices[this.minimalPrices.length - 1];
-            // console.log('Top minimal in else:\t', this.topMinimal.name, '\t', Math.floor(this.topMinimal.price));
         }
     }
 }
